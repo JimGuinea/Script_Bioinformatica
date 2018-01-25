99977f56de1e772b838387301b55ab62d5f52d6d
JimGuinea <28630905+JimGuinea@users.noreply.github.com>
2018-01-26 00:20:35 +0100

    Update Tokyo
    Aggiunte alcune opzioni con argumentParser

--- Tokyo.py
+++ Tokyo.py
@@ -29,66 +29,56 @@ import csv
 
 tempo_iniziale = time.time()
 
-parser = argparse.ArgumentParser(description='TLiMe: TSV Line Merger')
-parser.add_argument('-i', '--input', help = 'inserire il file TSV di input')
-parser.add_argument('-o', '--output', default = 'output.txt', help = 'inserire il file di output')
-#~ parser.add_argument('-C', '--locus_tag', default = 0, help ='inserire la colonna dei Locus Tag (PRIMA COLONNA = 0), di default PRIMA COLONNA (=0)')
-#~ parser.add_argument('-c', '--data', default = 1, help='inserire la colonna deid ati da ordinare (PRIMA COLONNA = 0), di default SECONDA COLONNA (=1)')
+parser = argparse.ArgumentParser(description='TLiMe: TSV Line Merger. Questo programma permette di unire tutti gli elementi appartenenti ad uno stesso locus tag in un file CSV')
+
+parser.add_argument('-i', '--input', help = 'inserire il file TSV di input', metavar='')
+parser.add_argument('-o', '--output', default = 'output.txt', help = 'inserire il file di output', metavar='')
+parser.add_argument('-d', '--divider', default = ',', help= 'divisore delle colonne per il file CSV di output (default= , )', metavar='')
+parser.add_argument('-D', '--Divider', default = '|', help= 'divisore per i terms della seconda colonna (default = | )', metavar='')
+parser.add_argument('-C', '--Character', default = ';', help = 'divisore delle colonne per file CSV di input (defailt= ;)', metavar='') 
 args = parser.parse_args()
+
 
 TSV_file = args.input
 output_file = args.output
+divider = args.divider
+Divisor = args.Divider
+char= args.Character
+
 output=[]
 locus_tag_precedente = ''
 term_precedente = []
 line_merged = []
-# ~ print('LOCUS TAG PRECEDENTE', locus_tag_precedente)
-# ~ print('TERM PRECEDENTE', term_precedente)
-#~ c1 = int(args.locus_tag)
-#~ c2 = int(args.data)
 
 def list_to_string(lista):
 	outString=[]
 	outString2=[]
 	str1=''
-	# ~ flag =False
 	for elem in lista:
 		outString.append(elem)
 		outString2 = [sublist for sublist in outString if any(sublist)] #ripulisce le sottoliste vuote contenute in una lista
-		# ~ print (outString2)
-		# ~ outString[:] = [item for item in outString if item != '']
-		str1='|'.join(str(r) for v in outString2 for r in v)
-		# ~ print(str1)
+		str1=Divisor.join(str(r) for v in outString2 for r in v) #unisce le sottoliste in una stringa dividendole con il carattere |
 	return str1
 	
 def print_output(output):
 	for row in output:
-		print(row[0],';',list_to_string(row[1]), file=fo)
+		print(row[0] + divider + list_to_string(row[1]), file=fo)
 
 
 flag_first = True
 with open (output_file, 'w', newline = '') as fo:
 	with open(TSV_file, 'r') as fi:
-		fi = csv.reader(fi, delimiter=';', quoting=csv.QUOTE_NONE)
+		fi = csv.reader(fi, delimiter=char, quoting=csv.QUOTE_NONE)
 		for line in fi:
-			line[1] = line[1].split('|')
-			# ~ line[1] = [i for i in line[1] if i != '']
-			# ~ for i in line[1]:
-				# ~ line_merged += i
+			line[1] = line[1].split(Divisor)
 
 			if flag_first:
 				locus_tag_precedente =line[0]
 				flag_first = False
 			if line[0] == locus_tag_precedente:
 				if line[1] != '':
-					# ~ print(line[1])
 					if not line[1] in term_precedente:
-						# ~ term_precedente = line[1].split('|')
 						term_precedente.append(line[1])
-						# ~ print(term_precedente)
-
-						# ~ term_precedente = '|'.join(line[1])
-						# ~ print(term_precedente)
 			else:
 				output.append([locus_tag_precedente, term_precedente])
 				term_precedente=[]
@@ -97,11 +87,12 @@ with open (output_file, 'w', newline = '') as fo:
 					if not line[1] in term_precedente:
 						term_precedente.append(line[1])
 		output.append([locus_tag_precedente, term_precedente])
+		print('RUNNING')
 		print_output(output)
 
 tempo_finale=time.time()
 tempo_esecuzione= tempo_finale-tempo_iniziale
-print('RUNNING')
+
 print('Tempo di esecuzione: ', tempo_esecuzione, 'secondi')
 
 
